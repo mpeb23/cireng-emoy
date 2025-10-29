37e81ae67b94196427ee22decade888c7ed1b7b0
mpeb23 <130307528+mpeb23@users.noreply.github.com>
2025-10-29 09:56:15 +0700

    save

--- script.js
+++ script.js
@@ -75,12 +75,3 @@ document.addEventListener('DOMContentLoaded', function() {
     animateOnScroll(); // Run once on load
 });
 
-
-window.addEventListener("scroll", () => {
-    const menuSection = document.querySelector("#menu");
-    if (window.scrollY > 100) {
-        menuSection.classList.add("scrolled");
-    } else {
-        menuSection.classList.remove("scrolled");
-    }
-});
